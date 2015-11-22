26c2f311f1b51e353dad1389c7b0b666d5c8cff6
Lei He <leihe023@gmail.com>
2015-11-22 10:58:03 +0000

    server fix

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.get('/evaluate_mp_using_text_test', function (req, res) {
 
 app.use('/home', express.static('public'));
 
-app.listen(3000);
-console.log("Server listening on port 3000");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
